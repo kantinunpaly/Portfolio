6bba63f2ec5117e06ea3dcd0a913227ec41f9cfc
kantinunpaly <palykantinun2002@gmail.com>
2024-07-12 18:33:35 +0700

    เพิ่ม sec3 แก้ไขรายละเอียด

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 
     // jQuery methods go here...
+    jQuery(document).ready(function(){
+      jQuery('.skillbar').each(function(){
+        jQuery(this).find('.skillbar-bar').animate({
+          width:jQuery(this).attr('data-percent')
+        },2000);
+      });
+    });
   
   });
